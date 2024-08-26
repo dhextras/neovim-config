a54c2cb485314fd082687b7a4995593e666fcfd0
dhextras <mohammedsafki27@gmail.com>
2024-08-26 10:11:49 +0530

    more configurations

--- lua/dhextras/plugins/autoformat.lua
+++ lua/dhextras/plugins/autoformat.lua
@@ -29,13 +29,14 @@ return {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        python = { 'pyright' },
+        python = { 'black' },
         --
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
         javascript = { 'prettier' },
         typescript = { 'prettier' },
+        json = { 'prettier' },
       },
     },
   },
